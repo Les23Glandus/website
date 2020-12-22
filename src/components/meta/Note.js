cdc3ebc10fdc1f6f268f8ff150341937bfebcc20
Laurent DURIEU <les23glandus@gmail.com>
2020-12-22 11:06:12 +0100

    node

--- src/components/meta/Note.js
+++ src/components/meta/Note.js
@@ -11,12 +11,13 @@ class Note extends React.Component {
 
 
     let v = this.props.value ? this.props.value : 3;
+    let div = Math.round( 10*( v % 1) );
 
     if( !this.props.value ) {
       return (<span/>)
     } else {
       return (
-      <span className={"note " + (this.props.light ? "light" : "dark")} title={Note.desc[Math.floor(v)]}>{v}{!this.props.compact && <i>/5</i>}</span>
+      <span className={"note " + (this.props.light ? "light" : "dark")} title={Note.desc[Math.floor(v)]}>{Math.floor(v)}{div!==0 && <span className="div">.{div}</span>}{!this.props.compact && <i>/5</i>}</span>
       )
     }
   }

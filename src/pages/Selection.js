44f6aef66b837a964f355e105cffbe3b1cf705aa
Laurent DURIEU <les23glandus@gmail.com>
2021-01-03 17:23:26 +0100

    fix selection nav

--- src/pages/Selection.js
+++ src/pages/Selection.js
@@ -19,7 +19,7 @@ class Selection extends React.Component {
     }
     return (
       <div>
-        <SelectionCompo selectionRef={this.props.match.params.selection}
+        <SelectionCompo key={this.props.match.params.selection}  selectionRef={this.props.match.params.selection}
          onError={()=>this.setState({error:true})}/>
       </div>
     );
